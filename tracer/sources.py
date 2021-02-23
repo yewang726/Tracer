55cac7840073531b17b2e4e5f4840f18a67748eb
casselineau <charles-alexis.asselineau@anu.edu.au>
2021-02-23 16:42:45 +1100

    Corrected an error in triangular ray bumdles.

--- tracer/sources.py
+++ tracer/sources.py
@@ -493,6 +493,7 @@ def triangular_bundle(num_rays, A, B, C, direction=None, ang_range=N.pi/2., flux
 	rayb.set_vertices(vertices)
 	rayb.set_directions(directions)
 
+	# Heron's formula for triangle surface area
 	l1 = N.sqrt(N.sum(AB**2))
 	l2 = N.sqrt(N.sum(AC**2))
 	l3 = N.sqrt(N.sum((-AB+AC)**2))
@@ -500,7 +501,7 @@ def triangular_bundle(num_rays, A, B, C, direction=None, ang_range=N.pi/2., flux
 	area = N.sqrt(s*(s-l1)*(s-l2)*(s-l3))
 	if flux != None:
 		cosangle = 2.*N.arcsin(0.5*N.sqrt(N.sum((direction-normal)**2)))
-		rayb.set_energy(area/2./num_rays*flux*N.ones(num_rays)*N.cos(cosangle))
+		rayb.set_energy(area/num_rays*flux*N.ones(num_rays)*N.cos(cosangle))
 	else:
 		rayb.set_energy(N.ones(num_rays)/float(num_rays)/procs)
 
